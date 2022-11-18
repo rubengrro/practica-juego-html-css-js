a8500ed7a375cfb651d8acc7d1f5bf7eaa7a15d0
Rubén Guerrero <rguerreroespinosa97@gmail.com>
2022-11-18 14:27:34 -0600

    Validación del mokepon seleccionado en js, condicionales if, else if y else

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,6 +1,19 @@
-
 function petSelector(){
-    alert('SELECCIONASTE TU MASCOTA')
+    if (document.getElementById('ratigueya').checked){
+        alert('Seleccionaste a Ratigueya')
+    } else if(document.getElementById('capipepo').checked){
+        alert('Seleccionaste a Capipepo')
+    }
+    else if (document.getElementById('hipodoge').checked){
+        alert('Seleccionaste a Hipodoge')
+    } else {
+        alert('No puedes iniciar sin un Mokepon')
+    }
 }
-let petSelectorBtn = document.getElementById('pet-selector-btn')
-petSelectorBtn.addEventListener('click', petSelector)
+function startGame(){
+    let petSelectorBtn = document.getElementById('pet-selector-btn')
+    petSelectorBtn.addEventListener('click', petSelector)
+}
+
+
+window.addEventListener('load', startGame)
